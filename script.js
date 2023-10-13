98e8e4279d1c2ec47600467423ba4402df8277b0
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2023-10-13 16:11:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots.sort((a,b)=>{
+	if(a>b) {return 1}
+	if(a<b) {return -1}
+	return 0;
+})
+function removeArticles(name) {
+  return touristSpots.replace(/^(a |an |the )/i, '').trim();
+}
 
