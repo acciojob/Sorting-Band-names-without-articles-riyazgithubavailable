5f937868f93a0d9d525dc88438c8fa7f4618c1cf
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-08-23 12:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 //your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort((a,b)=>{
-	if(a>b) {return 1}
-	if(a<b) {return -1}
-	return 0;
-})
-function removeArticles(name) {
-  return touristSpots.replace(/^(a |an |the )/i, '').trim();
+function getArticleLessString(input){
+	let words = input.split(" ");
+	let nonArticleWord = words.filter((word)=>{
+		let smallCase = word.toLowerCase();
+		if(smallCase === "the" || smallCase === "an" || smallCase === "a"){
+			return false;
+		}
+		return true;
+	})
+	return nonArticleWord.join(" ");
+}
+let mp ={};
+let arr1 = [];
+
+for(let i=0;i<touristSpots.length;i++){
+	let articleLessString = getArticleLessString(touristSpots[i]);
+	mp[articleLessString]=touristSpots[i];
+	arr1.push(articleLessString);
 }
 
+arr1.sort();
+let outputList = [];
+arr1.forEach((newString)=>{
+	outputList.push(mp[newString]);
+})
+const ul = document.getElementById("band");
+outputList.forEach((item)=>{
+	const li = document.createElement("li");
+	li.innerText = item;
+	ul.append(li);
+})
